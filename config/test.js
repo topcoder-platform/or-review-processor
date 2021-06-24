19963856abd3b94b3ace95394871cfea60fadbf0
Mithun Kamath <callmekatootie@gmail.com>
2021-06-24 19:41:03 +0530

    #19 - Fix tests

--- config/test.js
+++ config/test.js
@@ -4,5 +4,5 @@
 
 module.exports = {
   WAIT_TIME: process.env.WAIT_TIME ? Number(process.env.WAIT_TIME) : 2000,
-  SCORECARD_API_URL: 'https://api.topcoder-dev.com/scorecards'
+  SCORECARD_API_URL: 'https://api.topcoder-dev.com/v5/scorecards'
 }
